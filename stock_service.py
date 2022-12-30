44c67752a4cba5b8df6d9b009f420faad2bef9e5
Jocelyn Chang <jrc300@gmail.com>
2022-12-29 17:57:35 -0800

    added stock update request

--- stock_service.py
+++ stock_service.py
@@ -1,9 +1,12 @@
 import json
+import psycopg2
 
 from flask import Flask
 from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+
+from utils import get_formatted_date, scrape_for_stock_data
 
 app = Flask(__name__)
 
@@ -19,12 +22,7 @@ def get_stock_data_for_ticker(ticker):
     :param ticker: The CME symbol for a given stock ticker
     :return: json of data which includes [date, close/last, volume, open, high, low]
     """
-    service = Service(executable_path="/opt/homebrew/bin/chromedriver")
-    driver = webdriver.Chrome(service=service)
-    # driver = webdriver.Firefox(executable_path="/path/to/geckodrive.exe")
-    driver.get(CME_HISTORICAL_DATA_URL.format(ticker))
-
-    rows = driver.find_elements(By.XPATH, "//tr[@class='historical-data__row']")
+    rows = scrape_for_stock_data(CME_HISTORICAL_DATA_URL.format(ticker), "//tr[@class='historical-data__row']")
     rows_text = []
     for row in rows:
         rows_text.append(row.text)
@@ -34,6 +32,40 @@ def get_stock_data_for_ticker(ticker):
 def get_fed_schedule():
     pass
 
-@app.route("/update/<ticker>")
-def update_ticker():
-    pass
+@app.route("/stock_data/<ticker>/update")
+def update_ticker_postgres(ticker):
+    table_sql = f'CREATE TABLE IF NOT EXISTS public.{ticker.lower()} ' \
+                f'(date date NOT NULL, close money NOT NULL, volume integer NOT NULL, ' \
+                f'open money NOT NULL, high money NOT NULL, low money NOT NULL, ' \
+                f'CONSTRAINT "{ticker}_pkey" PRIMARY KEY (date)) ' \
+                f'TABLESPACE pg_default; ' \
+                f'ALTER TABLE IF EXISTS public.{ticker.lower()} ' \
+                f'OWNER to postgres;'
+
+    sql1 = f'INSERT INTO {ticker.lower()} (date, close, volume, open, high, low) VALUES (%s, %s, %s, %s, %s, %s) '
+    sql2 = f'ON CONFLICT DO NOTHING;'
+    sql = sql1 + sql2
+    try:
+        # Get data
+        rows = scrape_for_stock_data(CME_HISTORICAL_DATA_URL.format(ticker), "//tr[@class='historical-data__row']")
+
+        # Connect to your postgres DB
+        conn = psycopg2.connect("dbname=stonks user=postgres password=bonitis")
+        # Open a cursor to perform database operations
+        cur = conn.cursor()
+        cur.execute(table_sql, ())
+        # execute the INSERT statement
+        for row in rows:
+            r = row.text.split(' ')
+            formatted_date = get_formatted_date(r[0])
+            if r[2] == 'N/A':
+                volume = '0'
+            else:
+                volume = r[2].replace(',', '')
+            cur.execute(sql, (formatted_date, r[1], volume, r[3], r[4], r[5]))
+        # commit the changes to the database
+        conn.commit()
+        return f'Finished updating {ticker.upper()}'
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+        return error
